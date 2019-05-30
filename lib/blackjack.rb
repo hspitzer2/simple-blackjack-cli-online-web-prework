e2be3eb925eb11f6f36c9241c3500930083526c0
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-30 19:30:37 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,30 +1,39 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  deal = 1 + rand(11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+  
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
-def initial_round
-  # code #initial_round here
-end
+```def initial_round(card_one, card_two)
+  deal_card 
+  card_one =
+  deal_card
+  card_two =
+  display_card_total = #{card_1_one} + #{card_two}
+  display_card_total
+end```
+# deal_card method twice, use the 
+# display_card_total method to puts out the sum and then return the sum. This method will call on two other helper methods, #deal_card and #display_card_total, which takes in an argument of the sum of both invocations of #deal_card.
+
 
 def hit?
   # code hit? here
